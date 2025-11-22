b8faa12454aa934e54791e686c96220b16d854ff
sasha <charushinas@gmail.com>
2025-11-22 18:02:12 +0300

    added Main array

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main() {
+        // Массив из 100 элементов
+        int[] arr = new int[100];
+
+        // Инициализируем генератор случайных чисел
+        Random random = new Random();
+        // Инициализируем ArrayList indices
+        ArrayList<Integer> indices = new ArrayList<>();
+
+        // Заполняем массив сгенерированными значениями
+        for (int i = 0; i < arr.length; i++) {
+            // Случайное число от 75 до 83 включительно
+            arr[i] = 75 + random.nextInt(9);
+            indices.add(i);
+        }
+
+        // Печатаем сгенерированный массив
+        System.out.println("Все значения массива: " + indices);
+        // Очищаем ArrayList indices
+        indices.clear();
+
+        // Находим индексы ячеек, где хранится 79
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 79) {
+                indices.add(i);
+            }
+        }
+
+        // Индексы можно вывести
+        System.out.println("Индексы ячеек, где хранится 79: " + indices);
+    }
+}
